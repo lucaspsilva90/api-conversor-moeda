b919fdb6264b4ac3bc5fde34a4fac3cc9c4903a1
Lucas P. Silva <lucas.p.silva90@hotmail.com>
2020-10-01 20:04:25 -0300

    criação do projeto, configuração do bando de dados e criação das models: user, transaction e historic

---
+++ migrations/20201001225845-create-historic.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Historics', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_user: {
+        type: Sequelize.INTEGER
+      },
+      id_transaction: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Historics');
+  }
+};
